bfb5792b5e9af88821544c57ba52a22746b5306f
Owen Silva <owensilva1282@gmail.com>
2022-06-08 16:00:51 -0400

    Setup environment secrets

--- spot-sdk.test/Connection.cs
+++ spot-sdk.test/Connection.cs
@@ -1,21 +1,25 @@
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 
 namespace Sharks.Spot.Test
 {
     public class ConnectionTest
     {
+        IConfiguration? Configuration { get; set; }
         Robot? Robot;
 
         [SetUp]
         public void Setup()
         {
+            Configuration = new ConfigurationBuilder().AddUserSecrets<ConnectionTest>().Build();
+
             Robot = new Robot(
                 new ContactInfo() 
                 { 
                     Credentials = new RobotCredentials() 
                     { 
                         Username = "user",
-                        Password = "",
+                        Password = Configuration["SpotPass"],
                         Url = "192.168.80.3:443"
                     } 
                 },
@@ -27,6 +31,12 @@ namespace Sharks.Spot.Test
         public void Connect()
         {
             Assert.True(Robot?.Connect() == Result.Success);
+        }
+
+        [Test]
+        public void Initialise()
+        {
+            Assert.True(Robot?.Initialise() == Result.Success);
         }
     }
 }
